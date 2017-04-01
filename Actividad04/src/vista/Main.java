67a9ba73bbffed78efb46c71cb6395ef069a6931
Alberto <Alberto@Alberto>
2017-04-01 23:50:45 +0200

    Fin eje1

--- Actividad04/src/vista/Main.java
+++ Actividad04/src/vista/Main.java
@@ -1,5 +1,10 @@
 package vista;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import vista.mainPolinomio;
+
 public class Main {
 
 	/**
@@ -7,7 +12,43 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int seleccion = -1;
 
+		do {
+			System.out.println("***************************************");
+			System.out.println("Menú de operación:");
+			System.out.println("1- Ejercicio 1.");
+			System.out.println("2- Ejercicio 2.");
+			System.out.println("3- Ejercicio 3");
+			System.out.print("Seleccione operación:");
+			try {	
+				
+				seleccion =Integer.parseInt(bf.readLine());
+		
+				switch (seleccion)
+				{
+					case 1:
+						System.out.println("***************************************");
+						mainPolinomio.main(args);
+						break;
+					default:
+						System.out.println("Opción incorrecta");
+						break;
+				}
+			}
+			catch (Exception e)
+			{
+				System.out.println("Error");
+			}
+		} while (true);
+		
+		
+		
 	}
 
 }
